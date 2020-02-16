545684de2b5c3b84d98c3d106d2f3bc1117df5b7
Matt Robinson <git@nerdoftheherd.com>
2020-02-16 21:02:00 +0000

    Initial dropbear build script and workflow
    * Build script for cross-compiling dropbear for Android on Linux.
    * An initial localoptions.h to allow the compilation to succeed and
      speed up the binaries.
    * GitHub action to run the build script after each push and store the
      generated binaries as an artifact.

---
+++ localoptions.h
@@ -0,0 +1,13 @@
+/*
+ * Macro definitions for cross-compiling Dropbear for Android
+ * Copyright © 2020 Matt Robinson
+ */
+
+// Disable server password auth as crypt() isn't available under Android
+#define DROPBEAR_SVR_PASSWORD_AUTH 0
+
+// Disable client password auth as getpass() isn't available under Android
+#define DROPBEAR_CLI_PASSWORD_AUTH 0
+
+// Speed up symmetrical ciphers and hashes at the expense of larger binaries
+#define DROPBEAR_SMALL_CODE 0
