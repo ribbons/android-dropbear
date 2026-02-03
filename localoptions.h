9b72d01e9e64f865264af1741e12114c1c5478aa
Matt Robinson <git@nerdoftheherd.com>
2026-02-03 20:44:43 +0000

    Define USE_DEV_PTMX to try and fix pty allocation
    It was suggested in issue #86 that defining USE_DEV_PTMX does in fact
    fix pty allocation for the server under Android (despite the comment in
    the implementation that it doesn't work under Linux) so define this in
    localoptions.h.
    
    As this will not be needed when compiling with API level 23 or above
    (due to openpty() being available) add a conditional preprocessor error
    directive to ensure that this gets removed when we change to that.
    
    Hopefully resolves #62.

--- localoptions.h
+++ localoptions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2024 Matt Robinson
+ * Copyright © 2020-2026 Matt Robinson
  *
  * SPDX-License-Identifier: MIT
  */
@@ -18,3 +18,9 @@
 
 // Include non-standard shell path prior to Android 11 in default shell list
 #define COMPAT_USER_SHELLS "/bin/sh","/system/bin/sh"
+
+// Enable use of /dev/ptmx until openpty() is available
+#if __ANDROID_MIN_SDK_VERSION__ >= 23
+    #error "USE_DEV_PTMX define no longer needed as openpty() is now available"
+#endif
+#define USE_DEV_PTMX 1
